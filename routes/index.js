c8749f66584dd472cadef2d43b2eb8af73b2c7e1
Phil Salter <psalter@skydrop.com>
2018-07-12 14:48:46 -0600

    create middleware function to verify array that is submitted via the textarea.

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,49 @@ router.get('/', function(req, res, next) {
 });
 
 // POST for verifying dependency array
-router.post('', function (req, res){ 
-  console.log('I\'m in here baby!');
+router.post('', function (req, res, next){
+  console.log('before verifyData function', req.body.dependency_array);
+  verifyData(req, res, next);
+  console.log('after verifyData function', req.body.dependency_array);
   res.render('verified', { title: 'Results:', dependency_array: req.body.dependency_array });
 });
+
+function verifyData(req, res, next){
+  let finalArray = [];
+
+  let origContent = req.body.dependency_array.toString();
+  if(origContent.slice(-1) === ']'){
+    origContent = origContent.substring(0, origContent.length-1);
+  }
+  if(origContent.slice(-1) === "\""){
+    origContent = origContent.substring(0, origContent.length-1);
+  }
+  if(origContent.charAt(0) === '['){
+    origContent = origContent.substring(1);
+  }
+  if(origContent.charAt(0) === '\"'){
+    origContent = origContent.substring(1);
+  }
+  if(origContent.charAt(0) === ' '){
+    origContent = origContent.substring(1);
+  }
+  origContent.trim();
+  let array = origContent.split('", "');
+
+  console.log('array before verifying dependencies', array);
+
+  for(let x = 0; x < array.length; x++){
+    console.log('current array item:', array[x]);
+    let arrayItems = array[x].split(': ');
+    console.log('arrayItems are:', arrayItems);
+    if(arrayItems[1] === ''){
+      finalArray.push(arrayItems[0]);
+    }
+  }
+
+  req.body.dependency_array = finalArray;
+
+  next();
+};
 
 module.exports = router;
