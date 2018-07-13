d2a9d51958525bfdc8fb58912acc9e63c98bc95b
Phil Salter <psalter@skydrop.com>
2018-07-13 09:06:41 -0600

    Implement catching of dependency cycles

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 router.use(bodyParser.urlencoded({ extended: true }));
 
 // GET home page.
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Dependency Order Verifier' });
 });
 
 // POST for verifying dependency array
-router.post('', function (req, res, next){
+router.post('', (req, res, next) => {
   router.processArrayFromRequest(req, res, next);
   res.render('verified', { title: 'Results:', dependency_array: req.body.dependency_array });
 });
 
-router.processArrayFromRequest = function (req, res, next){
+router.processArrayFromRequest = (req, res, next) => {
   // Extract "array" from request before sending to verifyData function
   let origArray = req.body.dependency_array;
   req.body.dependency_array = router.verifyData(origArray);
@@ -22,10 +23,13 @@ router.processArrayFromRequest = function (req, res, next){
   next();
 }
 
-router.verifyData = function (content){
+router.verifyData = (content) => {
   let finalArray = [];
   let packageArray = [];
   let dependencyArray = [];
+  let packageDependencyObject = {};
+  let dependencyChain = [];
+  let dependencyCycleDetected = false;
 
   let origContent = content;
   // Remove brackets and quotation marks at both ends first
@@ -35,8 +39,8 @@ router.verifyData = function (content){
   let array = origContent.split('", "');
 
   // first remove any without dependencies
-  for(let x = 0; x < array.length; x++){
-    let arrayItems = array[x].split(': ');
+  for(let i = 0; i < array.length; i++){
+    let arrayItems = array[i].split(': ');
     if(arrayItems[1] === ''){
       finalArray.push(arrayItems[0]);
     } else {
@@ -46,23 +50,53 @@ router.verifyData = function (content){
     }
   }
 
-  // now add any dependencies as long as they aren't already in finalArray
-  for(let x = 0; x < dependencyArray.length; x++){
-    if(!finalArray.includes(dependencyArray[x])){
-      finalArray.push(dependencyArray[x]);
+  // build an object that represents all packages and dependencies as key value pairs
+  for(let i = 0; i < packageArray.length; i++){
+    packageDependencyObject[packageArray[i]] = dependencyArray[i];
+  }
+
+  // Add first package in packageArray to dependencyChain array
+  let addAnother = true;
+  dependencyChain.push(packageArray[0]);
+  // now add dependency of last item in the array until there are no more to add
+  while(addAnother){
+    if(dependencyChain.length <= packageArray.length){
+      addAnother = true;
+      dependencyChain.push(packageDependencyObject[dependencyChain[dependencyChain.length-1]]);
+    } else {
+      addAnother = false;
     }
   }
 
-  // now add any packages as long as they aren't already in finalArray
-  for(let x = 0; x < packageArray.length; x++){
-    if(!finalArray.includes(packageArray[x])){
-      finalArray.push(packageArray[x]);
-    }
+  // Remove any undefined items in array
+  while(dependencyChain[dependencyChain.length-1] === undefined){
+    dependencyChain.pop();
   }
 
-  return finalArray;
-
-  // next();
+  // Check for duplicates in dependencyChain array
+  let cleanDependencyChain = _.uniq(dependencyChain);
+  if(cleanDependencyChain.length < dependencyChain.length){
+    dependencyCycleDetected = true;
+  }
+  
+  // if there is not dependency cycle detected then go ahead and build final output
+  if(!dependencyCycleDetected){
+    // now add any dependencies as long as they aren't already in finalArray
+    for(let i = 0; i < dependencyArray.length; i++){
+      if(!finalArray.includes(dependencyArray[i])){
+        finalArray.push(dependencyArray[i]);
+      }
+    }
+    // now add any packages as long as they aren't already in finalArray
+    for(let i = 0; i < packageArray.length; i++){
+      if(!finalArray.includes(packageArray[i])){
+        finalArray.push(packageArray[i]);
+      }
+    }
+    return finalArray;
+  } else {
+    return Error('A dependency cycle has been detected! Please try again with a valid array.');
+  }
 };
 
 module.exports = router;
